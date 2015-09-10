b3a770814f5a8f849ee14dc4fc203b731150a793
YaneeSEnpru <yanee.moo.1995@gmail.com>
2015-09-10 21:00:47 +0700

    สร้าง getCustomerDetail.asmx.cs

---
+++ customer/customer/IgetCustomerDetail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+using System.Data.SqlClient;
+
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+// <System.Web.Script.Services.ScriptService()> _
+[System.Web.Services.WebService(Namespace = "http://tempuri.org/")]
+[System.Web.Services.WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[ToolboxItem(false)]
+public class getCustomerDetail : System.Web.Services.WebService
+{
+
+    [WebMethod()]
+    public DataTable DetailCustomer(string strCusID)
+    {
+        SqlConnection objConn = new SqlConnection();
+        SqlCommand objCmd = new SqlCommand();
+        SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+        DataSet ds = new DataSet();
+        DataTable dt = null;
+        string strConnString = null;
+        StringBuilder strSQL = new StringBuilder();
+
+        strConnString = "Server=localhost;UID=sa;PASSWORD=;database=mydatabase;Max Pool Size=400;Connect Timeout=600;";
+
+        strSQL.Append(" SELECT * FROM customer ");
+        strSQL.Append(" WHERE [CustomerID] = '" + strCusID + "' ");
+
+        objConn.ConnectionString = strConnString;
+        var _with1 = objCmd;
+        _with1.Connection = objConn;
+        _with1.CommandText = strSQL.ToString();
+        _with1.CommandType = CommandType.Text;
+        dtAdapter.SelectCommand = objCmd;
+
+        dtAdapter.Fill(ds);
+        dt = ds.Tables[0];
+
+        dtAdapter = null;
+        objConn.Close();
+        objConn = null;
+
+        return dt;
+
+    }
+
+}
